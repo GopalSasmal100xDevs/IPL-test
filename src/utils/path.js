cc172e805e60190286e79fb5d5ef8c338ccae58e
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-08 14:20:03 +0530

    Matchecs won per team par year in IPL

--- src/utils/path.js
+++ src/utils/path.js
@@ -8,7 +8,13 @@ export const matchesFilePath = path.join(__dirname, "../data/matches.csv");
 export const deliveriesJSONPath = path.join(__dirname, "../data/deliveries");
 export const matchesJSONPath = path.join(__dirname, "../data/matches");
 
-export const matchesPerYearPath = path.join(
-  __dirname,
-  "../public/output/matchesPerYear"
+const outputPath = "../public/output";
+
+export function generateOutputPath(fileName) {
+  return path.join(__dirname, `${outputPath}/${fileName}`);
+}
+
+export const matchesPerYearPath = generateOutputPath("matchesPerYear");
+export const matchesWonParTeamParYearPath = generateOutputPath(
+  "matchesWonParTeamParYear"
 );
