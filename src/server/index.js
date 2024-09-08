89bf2d0c997ca2ae5d7db7291606f39b3cae04f5
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-08 09:51:50 +0530

    Remove parse code from index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,40 +1,14 @@
-import fs from "fs";
-import { writeFileSync } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { parse } from "csv-parse";
+import { parseCSVFileToJSON } from "../utils/parseCSVtoJSON.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const deliveriesPath = path.join(__dirname, "../data/deliveries.csv");
-const matchesFilePath = path.join(__dirname, "../data/matches.csv");
-const deliveriesJSONPath = path.join(__dirname, "../data/deliveries");
-const matchesJSONPath = path.join(__dirname, "../data/matches");
+import {
+  deliveriesPath,
+  deliveriesJSONPath,
+  matchesFilePath,
+  matchesJSONPath,
+} from "../utils/path.js";
 
-function saveDataInFile(data, path) {
-  writeFileSync(`${path}.json`, JSON.stringify(data, null, 2));
+function main() {
+  parseCSVFileToJSON(deliveriesPath, deliveriesJSONPath);
+  parseCSVFileToJSON(matchesFilePath, matchesJSONPath);
 }
-
-function parseCSVFileToJSON(path, newFilePath) {
-  const data = [];
-  fs.createReadStream(path)
-    .pipe(
-      parse({
-        delimiter: ",",
-        columns: true,
-        ltrim: true,
-      })
-    )
-    .on("data", (row) => {
-      data.push(row);
-    })
-    .on("error", (error) => {
-      console.log(error.message);
-    })
-    .on("end", () => {
-      saveDataInFile(data, newFilePath);
-    });
-}
-
-parseCSVFileToJSON(deliveriesPath, deliveriesJSONPath);
-parseCSVFileToJSON(matchesFilePath, matchesJSONPath);
+main();
